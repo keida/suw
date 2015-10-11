d78e00c1707e1721e483592a35d64385606de829
keida <woohyuk0909@sina.com>
2015-10-12 00:06:32 +1300

    first angularjs project
    some bugs need fix it up.

---
+++ app/scripts/app.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict';
+  var app = angular.module('gemStore', ['store-directives','imgAnimations']);
+
+  app.controller('StoreController', ['$http', function($http){
+    var store = this;
+    store.products = [];
+    $http.get('./store-products.json').success(function(data){
+        store.products = data;
+    });
+  }]);
+
+  app.controller('ReviewController', function() {
+    this.review = {};
+
+    this.addReview = function(product) {
+      product.reviews.push(this.review);
+
+      this.review = {};
+    };
+  });
+})();
